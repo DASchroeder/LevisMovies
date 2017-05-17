631f8ae10fd374d168f0fd38d1271e13bf14548d
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-05-17 19:39:08 -0400

    Add project files.

---
+++ LevisMovies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LevisMovies.MvcApplication" Language="C#" %>
---
+++ LevisMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LevisMovies.Startup))]
+namespace LevisMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
